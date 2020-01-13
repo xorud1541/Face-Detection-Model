a76f0e65f7f846923f44df3103515319d192f619
전태경 <xorud1544@naver.com>
2020-01-13 23:22:41 +0900

    python argument 추가 argv[1] : 검색어 argv[2] : 다운받을 개수

--- Google-images-Crawler.py
+++ Google-images-Crawler.py
@@ -1,3 +1,7 @@
 from icrawler.builtin import GoogleImageCrawler
-google_crawler = GoogleImageCrawler(storage= {'root_dir': r'./temp'})
-google_crawler.crawl(keyword='레드벨벳 조이', max_num=300)
+import sys
+
+key_word = sys.argv[1]
+max_num = int(sys.argv[2])
+google_crawler = GoogleImageCrawler(storage= {'root_dir': key_word})
+google_crawler.crawl(keyword=key_word, max_num=max_num)
