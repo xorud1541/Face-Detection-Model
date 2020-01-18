b5e506ef0fc72874636eada952b1606b2d060e98
전태경 <xorud1544@naver.com>
2020-01-18 17:10:27 +0900

    test / train dataset 분할해서 훈련 및 평가하기 추후에 test에 새로운 사진 추가 가능

--- Detection-Model.py
+++ Detection-Model.py
@@ -3,17 +3,24 @@ import numpy as np
 import tensorflow as tf
 import pathlib
 import os
+import glob
 import matplotlib.pyplot as plt
 from PIL import Image
 from glob import glob
 from tensorflow.keras import datasets, layers, models
 
-data_dir = 'dataset'
+root_dir = 'dataset'
+data_dir = 'dataset/train'
+test_dir = 'dataset/test'
+
+root_dir = pathlib.Path(root_dir)
 data_dir = pathlib.Path(data_dir)
+test_dir = pathlib.Path(test_dir)
 
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 BATCH_SIZE = 10
-VAL_COUNT = 10
+TEST_COUT = 130
+TRAIN_COUT = 500
 CLASS_NAMES = np.array([item.name for item in data_dir.glob("*")])
 
 def get_label(file_path):
@@ -41,10 +48,14 @@ def prepare_for_training(ds, cache=True, shuffle_buffer_size=5):
 
     return ds
 
-list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'))
+train_list_ds = tf.data.Dataset.list_files(str(data_dir/'*/*'))
+test_list_ds = tf.data.Dataset.list_files(str(test_dir/'*/*'))
 
-labeled_ds = list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+labeled_ds = train_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
 train_ds = prepare_for_training(labeled_ds)
+
+labeled_test_ds = test_list_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+test_ds = prepare_for_training(labeled_test_ds)
 
 model = models.Sequential()
 model.add(layers.Conv2D(16, (3, 3), activation='relu', input_shape=(96, 96, 1)))
@@ -66,17 +77,32 @@ model.compile(optimizer=tf.optimizers.Adam(),
               loss=tf.keras.losses.binary_crossentropy,
               metrics=['accuracy'])
 
-steps_per_epoch = 500 / BATCH_SIZE
-model.fit(train_ds, epochs=20, steps_per_epoch=steps_per_epoch)
+steps_per_epoch = TRAIN_COUT / BATCH_SIZE
+steps_per_validation = TEST_COUT / BATCH_SIZE
+model.fit(train_ds, epochs=20, steps_per_epoch=steps_per_epoch, validation_data=test_ds, validation_steps=steps_per_validation)
 
-test_ds = train_ds.take(100)
-test_loss, test_acc = model.evaluate(test_ds)
+evaluate_ds = train_ds.take(100)
+test_loss, test_acc = model.evaluate(evaluate_ds)
 print(test_acc)
 
-for image, label in train_ds.take(1):
-    pred = model.predict(image)
-    print(np.argmax(pred[0]))
-    print(label[0])
+"""
+image, label = next(iter(train_ds))
+pred = model.predict(image)
+print(np.argmax(pred[0]))
+print(label[0])
+
+image = image.numpy()
+label = label.numpy()
+plt.figure(figsize=(10, 10))
+for n in range(10):
+    ax = plt.subplot(5, 5, n+1)
+    img = tf.reshape(image[n], [96, 96])
+    plt.imshow(img)
+    plt.title(CLASS_NAMES[label[n] == 1][0].title())
+    plt.axis('off')
+    plt.show()
+"""
+
 
 
 
