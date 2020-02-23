328fa78041a0c997cfdaf52c1d9427235dd41184
전태경 <xorud1544@naver.com>
2020-02-23 22:30:17 +0900

    image 경로를 input으로 주었을때, 해당 경로의 이미지를 예측하는 코드 추가

--- Redvelvet-Classifier.py
+++ Redvelvet-Classifier.py
@@ -1,21 +1,50 @@
 import tensorflow as tf
 import pathlib
 import numpy as np
+import sys
 import os
 from tensorflow.keras import datasets, layers, models
 
-#input-dir: /irene/1.png
-#image: 1.png
-#label: irene
+def decode_img(img):
+    img = tf.image.decode_jpeg(img, channels=1)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+    return img
 
-model_pb_dir = './dataset/model/saved_model.pb' #학습된 모델 경로
-model_pb_dir = pathlib.Path(model_pb_dir)
+input_path = sys.argv[1]
+print('input path:', input_path)
+
+image = tf.io.read_file(input_path)
+image = decode_img(image)
+
+label = input_path.split('\\')[-2]
+print('label', label)
+model_pb_dir = './dataset/model' #학습된 모델 경로
 
 print('model has loaded...')
 
 saved_model = tf.keras.models.load_model(model_pb_dir)
-predict_one_hot_array = saved_model.predict(#image)
+
+image = np.reshape(image, ((1,) + image.shape))
+predict_one_hot_array = saved_model.predict(image)
 predict_label = np.argmax(predict_one_hot_array)
+
+if predict_label == 0:
+    predict_label = 'irene'
+elif predict_label == 1:
+    predict_label = 'joy'
+elif predict_label == 2:
+    predict_label = 'seulgi'
+elif predict_label == 3:
+    predict_label = 'wendy'
+else:
+    predict_label = 'yeri'
+
+print('predict: ', predict_label)
+if predict_label == label:
+    print('correct!')
+else:
+    print('incorrect!')
+
 
 
 
