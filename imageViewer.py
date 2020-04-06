ce6fe9b1443e588b1130d12895494858d7c5fb05
전태경 <xorud1544@naver.com>
2020-04-06 23:45:07 +0900

    왼쪽, 오른쪽 버튼 클릭 시 해당 디렉토리 이미지들 네비게이팅 추가

--- imageViewer.py
+++ imageViewer.py
@@ -1,34 +1,56 @@
 import sys
+import os
+from PIL import Image
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import QIcon, QPixmap
 
-class MainWindow(QMainWindow):
+class ImageView(QMainWindow):
+    currentIndex = 0
+    imageList = []
+    dirName = 'dataset/image'
+    imageLabel = QLabel
     def __init__(self):
         super().__init__()
-        self.setGeometry(100, 200, 600, 550)
+        self.setGeometry(100, 200, 530, 600)
         self.setWindowTitle("ImageViewer")
 
         leftBtn = QPushButton("left", self)
-        leftBtn.setGeometry(200, 500, 100, 30)
+        leftBtn.setGeometry(170, 550, 100, 30)
         leftBtn.clicked.connect(self.onClickedLeftBtn)
 
         rigthBtn = QPushButton("right", self)
-        rigthBtn.setGeometry(300, 500, 100, 30)
+        rigthBtn.setGeometry(270, 550, 100, 30)
         rigthBtn.clicked.connect(self.onClickedRightBtn)
 
-        label = QLabel(self)
-        pixmap = QPixmap("./dataset/test/irene/1.jpg")
-        label.setPixmap(pixmap)
-        label.setGeometry(100, 100, 300, 300)
+        fileNames = os.listdir(self.dirName)
+        for fileName in fileNames:
+            full_fileName = os.path.join(self.dirName, fileName)
+            image = QPixmap(full_fileName)
+            image = image.scaledToWidth(512)
+            image = image.scaledToWidth(512)
+            self.imageList.append(image)
+
+
+        self.imageLabel = QLabel(self)
+        pixmap = self.imageList[self.currentIndex]
+        self.imageLabel.setPixmap(pixmap)
+        self.imageLabel.setGeometry(10, 10, 512, 512)
 
     def onClickedLeftBtn(self):
-        print("왼쪽 버튼 클릭")
+        self.currentIndex = self.currentIndex - 1
+        pixmap = self.imageList[self.currentIndex]
+        self.imageLabel.setPixmap(pixmap)
+        self.imageLabel.setGeometry(10, 10, 512, 512)
 
     def onClickedRightBtn(self):
-        print("오른쪽 버튼 클릭")
+        self.currentIndex = self.currentIndex + 1
+        pixmap = self.imageList[self.currentIndex]
+        self.imageLabel.setPixmap(pixmap)
+        self.imageLabel.setGeometry(10, 10, 512, 512)
+
 
 
 app = QApplication(sys.argv)
-window = MainWindow()
+window = ImageView()
 window.show()
 app.exec_()
